4705e6221aca0c3747f09642a7f4f05aaf6a4b83
fandul <rihards.pacevics@gmail.com>
2014-11-12 20:40:36 +0100

    control class
    added some return statments so it could compile

--- Flow Solid/src/controller/Control.java
+++ Flow Solid/src/controller/Control.java
@@ -2,17 +2,17 @@
 package controller;
 
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Control implements WordPairControlInterface {
 
-   ArrayList<WordPair> pairs = new ArrayList<WordPair>();
+  private ArrayList<WordPair> pairs = new ArrayList<WordPair>();
     
-    //WordStoring pairs = new WordStoring();
-    //pairs.add();
     @Override
     public void add(String question, String answer) {
-    pairs.add(new WordPair(question, answer));
+    
+        pairs.add(new WordPair(question, answer));
     }
 
     @Override
@@ -22,27 +22,27 @@ public class Control implements WordPairControlInterface {
 
     @Override
     public String getRandomQuestion() {
-
+        return null;
     }
 
     @Override
     public boolean checkGuess(String question, String quess) {
-
+        return false ;
     }
 
     @Override
     public String lookup(String question) {
-
+        return null;
     }
 
     @Override
     public boolean load(String filename) {
-
+        return false;
     }
 
     @Override
     public boolean save(String filename) {
-
+        return false;
     }
 
     @Override
