71b166b1b580e78e40a6fb77499ecea7d9df1cdd
fandul <rihards.pacevics@gmail.com>
2014-11-12 09:57:22 +0100

    changes

--- Flow Solid/src/controller/Control.java
+++ Flow Solid/src/controller/Control.java
@@ -1,17 +1,18 @@
 
 package controller;
 
-import flow.solid.WordPairControlInterface;
-import utilities.WordStoring;
+
+import java.util.Hashtable;
 
 public class Control implements WordPairControlInterface {
 
-    WordStoring pairs = new WordStoring();
-    pairs.add();
+    Hashtable<String,String> pairs = new Hashtable<String,String>();
     
+    //WordStoring pairs = new WordStoring();
+    //pairs.add();
     @Override
     public void add(String question, String answer) {
-     
+     pairs.put(new WordPair(answer, answer));
     }
 
     @Override
---
+++ Flow Solid/src/controller/WordStoring.java
@@ -0,0 +1,15 @@
+
+package controller;
+
+
+import java.util.Hashtable;
+
+public class WordStoring {
+    
+    private Hashtable<String,String> pairs = new Hashtable<String,String>();
+    
+    public void add(){
+    pairs.put("bil","mašīna");
+    }
+    
+}
